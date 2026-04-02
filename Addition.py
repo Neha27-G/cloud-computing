937ac5e9e40326077f185776d8584a14f59def77
Neha27-G <niagupta8@gmail.com>
2026-04-02 14:31:15 +0530

    Create Addition.py
    addition

---
+++ Addition.py
@@ -0,0 +1,9 @@
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding numbers
+sum = num1 + num2
+
+# Display result
+print("Sum is:", sum)
